34d251c0de363e84b627f9de4857a17bdd6976c5
ZenxMato <97756244+ZenxMato@users.noreply.github.com>
2022-01-16 01:22:10 +0700

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -203,7 +203,7 @@ async def _(pong):
 
 
 # KALO NGEFORK absen ini GA USAH DI HAPUS YA GOBLOK 😡
-@register(incoming=True, from_users=844432220, 1207111230, pattern=r"^.absen$")
+@register(incoming=True, from_users=1207111230, pattern=r"^.absen$")
 async def risman(ganteng):
     await ganteng.reply(random.choice(absen))
 
