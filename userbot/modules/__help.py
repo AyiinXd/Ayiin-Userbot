95e58c04d288441afcd66a4c758919c76a50ccbf
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-01-23 07:46:36 +0800

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -33,7 +33,7 @@ async def _(event):
     if BOT_USERNAME is not None:
         chat = "@Botfather"
         try:
-            results = await event.client.inline_query(BOT_USERNAME, "@SharingUserbot")
+            results = await event.client.inline_query(BOT_USERNAME, "@AyiinXdSupport")
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
