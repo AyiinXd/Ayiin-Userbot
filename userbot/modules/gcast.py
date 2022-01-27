a5b862ab455ed6d71d7554f154872c9a794b37f7
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-01-27 18:05:34 +0800

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -19,9 +19,23 @@ from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, DEVS
 from userbot.utils import edit_delete, edit_or_reply, man_cmd
 
-GCAST_BLACKLIST = get(
-    "https://raw.githubusercontent.com/mrismanaziz/Reforestation/master/blacklistgcast.json"
-).json()
+GCAST_BLACKLIST = [
+    -1001675396283,  # AyiinXdSupport
+    -1001473548283,  # SharingUserbot
+    -1001433238829,  # TedeSupport
+    -1001476936696,  # AnosSupport
+    -1001327032795,  # UltroidSupport
+    -1001294181499,  # UserBotIndo
+    -1001419516987,  # VeezSupportGroup
+    -1001209432070,  # GeezSupportGroup
+    -1001296934585,  # X-PROJECT BOT
+    -1001481357570,  # UsergeOnTopic
+    -1001459701099,  # CatUserbotSupport
+    -1001109837870,  # TelegramBotIndonesia
+    -1001752592753,  # Skyzusupport
+    -1001788983303,  # KayzuSupport
+    -1001380293847,  # NastySupport
+]
 
 
 @man_cmd(pattern="gcast(?: |$)(.*)")
