3d92d7c600469a11d11144b1f1969b9fe8da8cfc
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-01-29 08:11:31 +0800

    Create yinsubot2.py

---
+++ userbot/modules/yinsubot2.py
@@ -0,0 +1,123 @@
+# Copyright (C) 2021 Kyy - Userbot
+# Created by Kyy
+# Jangan hapus credit Anj!!!
+
+
+from time import sleep
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern="^.lipkol(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(2)
+    await typew.edit("`𝘼𝙔𝘼𝘼𝘼𝘼𝙉𝙂 🥺`")
+    sleep(2)
+    await typew.edit("`𝙆𝘼𝙉𝙂𝙀𝙀𝙀𝙉𝙉𝙉 👉👈`")
+    sleep(1)
+    await typew.edit("`𝙋𝙄𝙉𝙂𝙄𝙉 𝘾𝙇𝙄𝙋𝙆𝙊𝙇 𝙔𝘼𝘼𝘼𝙉𝙂𝙂𝙂 🥺👉👈`")
+
+
+# Create by myself @localheart
+
+
+@register(outgoing=True, pattern="^.nakal(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`𝘼𝙮𝙖𝙖𝙖𝙖𝙣𝙜 𝙄𝙝 🥺`")
+    sleep(1)
+    await typew.edit("`𝙉𝙖𝙠𝙖𝙡 𝘽𝙖𝙣𝙜𝙚𝙩 𝘿𝙖𝙝 𝘼𝙮𝙖𝙣𝙜 🥺`")
+    sleep(1)
+    await typew.edit("`𝘼𝙠𝙪 𝙂𝙖𝙠 𝙇𝙞𝙠𝙚 𝘼𝙮𝙖𝙖𝙖𝙣𝙜 😠`")
+    sleep(1)
+    await typew.edit("`𝙋𝙤𝙠𝙤𝙠𝙣𝙮𝙖 𝘼𝙠𝙪 𝙂𝙖 𝙇𝙞𝙠𝙚 𝙄𝙝 😠`")
+
+
+@register(outgoing=True, pattern=r"^\.favboy(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1.5)
+    await typew.edit("𝘿𝙪𝙪𝙪𝙝𝙝 𝘼𝙙𝙖 𝘾𝙤𝙬𝙤 𝙂𝙖𝙣𝙩𝙚𝙣𝙜 👉👈")
+    sleep(1.5)
+    await typew.edit("𝙔𝙤𝙪 𝘼𝙧𝙚 𝙈𝙮 𝙁𝙖𝙫𝙤𝙧𝙞𝙩𝙚 𝘽𝙤𝙮 😍")
+    sleep(1.5)
+    await typew.edit("𝙆𝙖𝙢𝙪 𝙃𝙖𝙧𝙪𝙨 𝙅𝙖𝙙𝙞 𝘾𝙤𝙬𝙤 𝘼𝙠𝙪 𝙔𝙖 😖")
+    sleep(1.5)
+    await typew.edit("𝙋𝙤𝙠𝙤𝙠𝙣𝙮𝙖 𝙃𝙖𝙧𝙪𝙨 𝙅𝙖𝙙𝙞 𝘾𝙤𝙬𝙤 𝘼𝙠𝙪 👉👈")
+    sleep(1.5)
+    await typew.edit("𝙂𝙖𝙠 𝘽𝙤𝙡𝙚𝙝 𝘼𝙙𝙖 𝙔𝙜 𝙇𝙖𝙞𝙣 😠")
+
+
+@register(outgoing=True, pattern="^.favgirl(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`𝘿𝙪𝙪𝙪𝙝𝙝𝙝 𝘼𝙙𝙖 𝘾𝙚𝙬𝙚 𝘾𝙖𝙣𝙩𝙞𝙠 👉👈`")
+    sleep(2)
+    await typew.edit("`𝙔𝙤𝙪 𝘼𝙧𝙚 𝙈𝙮 𝙁𝙖𝙫𝙤𝙧𝙞𝙩𝙚 𝙂𝙞𝙧𝙡 😍`")
+    sleep(2)
+    await typew.edit("`𝙆𝙖𝙢𝙪 𝙃𝙖𝙧𝙪𝙨 𝙅𝙖𝙙𝙞 𝘾𝙚𝙬𝙚 𝘼𝙠𝙪 𝙔𝙖 😖`")
+    sleep(2)
+    await typew.edit("`𝙋𝙤𝙠𝙤𝙠𝙣𝙮𝙖 𝙃𝙖𝙧𝙪𝙨 𝙅𝙖𝙙𝙞 𝘾𝙚𝙬𝙚 𝘼𝙠𝙪 👉👈`")
+    sleep(2)
+    await typew.edit("`𝙂𝙖𝙠 𝘽𝙤𝙡𝙚𝙝 𝘼𝙙𝙖 𝙔𝙖𝙣𝙜 𝙇𝙖𝙞𝙣 😠`")
+
+
+
+@register(outgoing=True, pattern="^.canlay(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(2)
+    await typew.edit("𝙀𝙝 𝙆𝙖𝙢𝙪 𝘾𝙖𝙣𝙩𝙞𝙠-𝘾𝙖𝙣𝙩𝙞𝙠")
+    sleep(2)
+    await typew.edit("𝙆𝙤𝙠 𝘼𝙡𝙖𝙮 𝘽𝙖𝙣𝙜𝙚𝙩 𝙎𝙞𝙝")
+    sleep(2)
+    await typew.edit("𝙎𝙥𝙖𝙢 𝘽𝙤𝙩 𝙈𝙪𝙡𝙪")
+    sleep(2)
+    await typew.edit("𝘽𝙖𝙧𝙪 𝙅𝙖𝙙𝙞 𝙐𝙨𝙚𝙧𝙗𝙤𝙩 𝙮𝙖??")
+    sleep(2)
+    await typew.edit("𝙋𝙖𝙣𝙩𝙚𝙨 𝙉𝙤𝙧𝙖𝙠 𝙓𝙞𝙭𝙞𝙭𝙞𝙭𝙞")
+
+
+@register(outgoing=True, pattern="^.ganlay(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(2)
+    await typew.edit("𝙀𝙝 𝙆𝙖𝙢𝙪 𝙂𝙖𝙣𝙩𝙚𝙣𝙜-𝙂𝙖𝙣𝙩𝙚𝙣𝙜")
+    sleep(2)
+    await typew.edit("𝙆𝙤𝙠 𝘼𝙡𝙖𝙮 𝘽𝙖𝙣𝙜𝙚𝙩 𝙎𝙞𝙝")
+    sleep(2)
+    await typew.edit("𝙎𝙥𝙖𝙢 𝘽𝙤𝙩 𝙈𝙪𝙡𝙪")
+    sleep(2)
+    await typew.edit(""𝘽𝙖𝙧𝙪 𝙅𝙖𝙙𝙞 𝙐𝙨𝙚𝙧𝙗𝙤𝙩 𝙮𝙖??")
+    sleep(2)
+    await typew.edit("𝙋𝙖𝙣𝙩𝙚𝙨 𝙉𝙤𝙧𝙖𝙠 𝙓𝙞𝙭𝙞𝙭𝙞𝙭𝙞")
+
+
+@register(outgoing=True, pattern="^.ange(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`𝘼𝙮𝙖𝙣𝙜𝙜𝙜𝙜😖`")
+    sleep(1)
+    await typew.edit("`𝘼𝙠𝙪 𝘼𝙣𝙜𝙚😫`")
+    sleep(1)
+    await typew.edit("`𝘼𝙮𝙪𝙠𝙠𝙠 𝙀𝙬𝙚𝙖𝙣 𝙔𝙖𝙣𝙜𝙜𝙜🤤`")
+
+
+
+CMD_HELP.update({
+    "yinsubot2": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.lipkol`\
+    \n↳ : Bucin Lipkol\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.nakal`\
+    \n↳ : Ga Like Ayang Nakal\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.favboy`\
+    \n↳ : You Are My Favorite Boy\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.favgirl`\
+    \n↳ : You Are My Favorite Girl\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.canlay`\
+    \n↳ : Ngatain Si Cantik alay\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ganlay`\
+    \n↳ : Ngatain Si Ganteng Alay\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ange`\
+    \n↳ : Sangean."
+})
