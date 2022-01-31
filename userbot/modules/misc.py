4c42c890d5b7ec956af445cf6c66e585b75eee6b
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-01-31 18:30:36 +0800

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -118,6 +118,7 @@ async def repeat(event):
 @man_cmd(pattern="repo$")
 async def repo_is_here(event):
     xx = await edit_or_reply(event, "😈")
+    sleep(2)
     await xx.edit(
         "**Hey**, I am using **᯽ 𝙰𝚈𝙸𝙸𝙽-𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ᯽** \n"
         "卍━━━━━━━━━━━━━━━━━━━━━━卍\n"
