aa915107a48c22bb84e5ac4d0ea5108c8766a290
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-02-04 08:56:03 +0800

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -28,7 +28,7 @@ async def help(event):
         for i in CMD_HELP:
             string += "`" + str(i)
             string += f"`\t\t\t{ICON_HELP}\t\t\t"
-        await edit_or_reply(🗿)
+        await edit_or_reply('🗿')
         sleep(3)
         await edit_or_reply(
             event,
