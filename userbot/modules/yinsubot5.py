86da50b02469758b0bb3f75ea7fc5bd15e77e701
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-02-04 11:14:23 +0800

    Create yinsubot5.py

---
+++ userbot/modules/yinsubot5.py
@@ -0,0 +1,84 @@
+from time import sleep
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+
+
+
+@register(outgoing=True, pattern='^.ganteng(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("`Lu Mau Tau Sebuah Fakta?`")
+    sleep(3)
+    await typew.edit("`Fakta Yang Belum Terbongkar Selama Ini`")
+    sleep(3)
+    await typew.edit("`**GUA GANTENG FIX NO DEBAT😏**`")
+
+
+
+@register(outgoing=True, pattern='^.wibu(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("`Kata Emak`")
+    sleep(2)
+    await typew.edit("`Kalo Ketemu Wibuu`")
+    sleep(3)
+    await typew.edit("`Harus Lari Sekenceng Mungkin🏃🏻`")
+    sleep(3)
+    await typew.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻`")
+    await typew.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨`")
+    await typew.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤ`")
+    await typew.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤ`")
+    await typew.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤ`")
+    await typew.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤ`")
+    await typew.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤ`")
+    await typew.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤ`")
+    await typew.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤ`")
+    await typew.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await typew.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await typew.edit("`ㅤㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await typew.edit("`ㅤㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await typew.edit("`ㅤㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await typew.edit("`ㅤㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await typew.edit("`ㅤㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await typew.edit("`ㅤㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await typew.edit("`ㅤㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await typew.edit("`ㅤ🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await typew.edit("`🏃🏻💨ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`")
+    await typew.edit("`🧎🏻‍♂️ huhh... akhirnya bisa lolos dari wibu mematikan`")
+
+# create by ayiin
+
+
+@register(outgoing=True, pattern='^.senggol(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(2)
+    await typew.edit("`Bapaknya Udin Di Makan Singkong`")
+    sleep(2)
+    await typew.edit("`Cuma Sendiri ni Senggol Dong`")
+
+
+@register(outgoing=True, pattern='^p(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("`Awali perkataan dengan dengan salam`")
+    sleep(1)
+    await typew.edit("`Assalamualaikum Sayang`")
+
+
+CMD_HELP.update({
+    "yinsubot5":
+    "CMD➣`.senggol`\
+\nPenjelasan: Cek lah asw.\
+\n\nCMD➣`p`\
+\nPenjelasan: Cek lah asw.\
+\n\nCMD➣`.wibu`\
+\nPenjelasan: Lari Dari Wibu.\
+\n\nCMD➣`.ganteng`\
+\nPenjelasan: Gua Ganteng."
+})
