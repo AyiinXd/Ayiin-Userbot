51f6db930a1db3b38217f8b3990bba24f578fb99
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-02-06 03:52:02 +0800

    Create yinstoxic2.py

---
+++ userbot/modules/yinstoxic2.py
@@ -0,0 +1,151 @@
+from time import sleep
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern='^.ngentot(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("**WOYY NGENTOD!!**")
+    sleep(1)
+    await typew.edit("**JANGAN SOK JAGOAN DAH LU**")
+    sleep(1)
+    await typew.edit("**MUKA MASIH KAYA KONTOL AJA**")
+    sleep(1)
+    await typew.edit("**BANGGA LU HAHAHAHA**")
+    sleep(1)
+    await typew.edit("**COBA DEH NGACA MUKA LU KAN HINA BANGET**")
+    sleep(1)
+    await typew.edit("**HAHAHAHA**")
+    sleep(1)
+    await typew.edit("**MAKANYA GANTENG KONTOL**")
+    sleep(1)
+    await typew.edit("**BIAR MUKALU GAK DIHINA TERUS**")
+    sleep(1)
+    await typew.edit("**SAMA ORANG LAIN**")
+    sleep(1)
+    await typew.edit("**HAHAHAHA**")
+# Create by myself @localheart
+
+
+@register(outgoing=True, pattern='^.goblok(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("**WOYY GOBLOK!!**")
+    sleep(1)
+    await typew.edit("**KO LU GOBLOK BANGET SIH**")
+    sleep(1)
+    await typew.edit("**OTAK LU TUH KAYA KONTOL**")
+    sleep(1)
+    await typew.edit("**YANG LEMBEK KETIKA LEMAH**")
+    sleep(1)
+    await typew.edit("**DAN KERAS KETIKA LU SANGE GOBLOK**")
+    sleep(1)
+    await typew.edit("**HAHAHAHA**")
+    sleep(1)
+    await typew.edit("**MAKANYA JANGAN SANGEAN MULU**")
+    sleep(1)
+    await typew.edit("**MUKA LU AJA KAYA ASPAL JALANAN**")
+    sleep(1)
+    await typew.edit("**EHHH SANGE NYA MAU DAPAT YANG CANTIK**")
+    sleep(1)
+    await typew.edit("**HAHAHAHA**")
+# Create by myself @localheart
+
+
+@register(outgoing=True, pattern='^.ngatain(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("**BABI!!**")
+    sleep(1)
+    await typew.edit("**MUKA LU KAYA BABI**")
+    sleep(1)
+    await typew.edit("**OTAK LU TUH KAYA KONTOL**")
+    sleep(1)
+    await typew.edit("**MUKA LU HINA BANGET**")
+    sleep(1)
+    await typew.edit("**OTAK LU KAYA BATU**")
+    sleep(1)
+    await typew.edit("**HAHAHAHA**")
+    sleep(1)
+    await typew.edit("**MAKANYA JANGAN SANGEAN MULU**")
+    sleep(1)
+    await typew.edit("**KONTOL LU AJA MASIH BENGKOK**")
+    sleep(1)
+    await typew.edit("**EHHH SANGE NYA MAU DAPAT YANG CANTIK**")
+    sleep(1)
+    await typew.edit("**HAHAHAHA**")
+# Create by myself @localheart
+
+
+@register(outgoing=True, pattern='^.yatim(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("`Hai Anak Kontol 🙈, Jangan Lupa Makan Yaa`")
+    sleep(1)
+    await typew.edit("`Jangan Bilang Lu Ga Dikasih Makan Sama Ortu 😁`")
+    sleep(1)
+    await typew.edit("`APA PERLU GUA SANTUNIN ?? 🙈🙈 xixixi`")
+    sleep(1)
+    await typew.edit("`OH IYAA LUPAAA, LU KAN BEBAN KELUARGA 🤣`")
+    sleep(1)
+    await typew.edit("`MANA MUNGKIN ORTU LU PEDULII xixixi 🙈`")
+    sleep(1)
+    await typew.edit("`KETAWA DULU BOLEH KALI YAA 😁`")
+    sleep(1)
+    await typew.edit("`HAHAHAHAHAHAHA`")
+    sleep(1)
+    await typew.edit("`KASIAN ORTUNYAA GAPEDULIII 🙈🤣`")
+    sleep(1)
+    await typew.edit("`MAAF YA, CANDAA BEBANNNN xixixi 🙈`")
+    sleep(1)
+    await typew.edit("`Tapi Bo'ong Hiyahiyahiya`")
+# Create by myself @localheart
+
+
+@register(outgoing=True, pattern=r"^\.kont(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("**KONTOLL**")
+    sleep(1.5)
+    await typew.edit("**LU ANAK KONTOLL**")
+    sleep(1.5)
+    await typew.edit("**DI BIKIN DARI KONTOLL**")
+    sleep(1.5)
+    await typew.edit("**MUKALU PERSIS KONTOLL**")
+    sleep(1.5)
+    await typew.edit("**DASAR ANAK NGONTOLLLL**")
+    sleep(1.5)
+    await typew.edit("**NOLEP KONTOLL**")
+    sleep(1.5)
+    await typew.edit("**NGERUSUH KONTOLL**")
+    sleep(1.5)
+    await typew.edit("**BENER BENER KONTOLL**")
+    sleep(1.5)
+    await typew.edit("**PADAHAL LO GAPUNYA KONTOLL**")
+    sleep(1.5)
+    await typew.edit("**MENDING LO OPERASI KONTOLL**")
+    sleep(1.5)
+    await typew.edit("**BIAR LO PUNYA KONTOLL**")
+    sleep(1.5)
+    await typew.edit("**KASIAN CACAD GAPUNYA KONTOLL**")
+
+
+CMD_HELP.update({
+    "yinstoxic2": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ngentot`\
+    \n↳ : Cobain aja\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.goblok`\
+    \n↳ : Cobain aja\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ngatain`\
+    \n↳ : Cobain aja\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.kont`\
+    \n↳ : Cobain aja\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.yatim`\
+    \n↳ : Cobain aja."
+})
