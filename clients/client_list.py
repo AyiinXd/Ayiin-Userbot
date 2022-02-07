d79988baf421a6f9fb9e65a53106a5ccba69f0c5
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-02-07 11:08:59 +0800

    Update client_list.py

--- clients/client_list.py
+++ clients/client_list.py
@@ -9,35 +9,35 @@ import telethon.utils
 from telethon.tl.functions.users import GetFullUserRequest
 
 
-async def clients_list(SUDO_USERS, bot, MAN2, MAN3, MAN4, MAN5):
+async def clients_list(SUDO_USERS, bot, AYIIN2, AYIIN3, AYIIN4, AYIIN5):
     user_ids = list(SUDO_USERS) or []
     main_id = await bot.get_me()
     user_ids.append(main_id.id)
 
     try:
-        if MAN2 is not None:
-            id2 = await MAN2.get_me()
+        if AYIIN2 is not None:
+            id2 = await AYIIN2.get_me()
             user_ids.append(id2.id)
     except BaseException:
         pass
 
     try:
-        if MAN3 is not None:
-            id3 = await MAN3.get_me()
+        if AYIIN3 is not None:
+            id3 = await AYIIN3.get_me()
             user_ids.append(id3.id)
     except BaseException:
         pass
 
     try:
-        if MAN4 is not None:
-            id4 = await MAN4.get_me()
+        if AYIIN4 is not None:
+            id4 = await AYIIN4.get_me()
             user_ids.append(id4.id)
     except BaseException:
         pass
 
     try:
-        if MAN5 is not None:
-            id5 = await MAN5.get_me()
+        if AYIIN5 is not None:
+            id5 = await AYIIN5.get_me()
             user_ids.append(id5.id)
     except BaseException:
         pass
@@ -49,11 +49,11 @@ async def client_id(event, botid=None):
     if botid is not None:
         uid = await event.client(GetFullUserRequest(botid))
         OWNER_ID = uid.user.id
-        MAN_USER = uid.user.first_name
+        AYIIN_USER = uid.user.first_name
     else:
         client = await event.client.get_me()
         uid = telethon.utils.get_peer_id(client)
         OWNER_ID = uid
-        MAN_USER = client.first_name
-    man_mention = f"[{MAN_USER}](tg://user?id={OWNER_ID})"
-    return OWNER_ID, MAN_USER, man_mention
+        AYIIN_USER = client.first_name
+    ayiin_mention = f"[{AYIIN_USER}](tg://user?id={OWNER_ID})"
+    return OWNER_ID, AYIIN_USER, ayiin_mention
