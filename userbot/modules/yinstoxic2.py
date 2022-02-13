20b2ad7d026355c194cda92ba84544113a93d39d
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-02-13 17:15:46 +0800

    Update yinstoxic2.py

--- userbot/modules/yinstoxic2.py
+++ userbot/modules/yinstoxic2.py
@@ -1,151 +1,149 @@
 from time import sleep
+
+from userbot import BLACKLIST_CHAT
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP
-from userbot.events import register
+from userbot import CMD_HELP, bot
+from userbot.events import ayiin_cmd
 
 
-@register(outgoing=True, pattern='^$ngentot(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
+
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"ngentot(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**WOYY NGENTOD!!**")
     sleep(1)
-    await typew.edit("**WOYY NGENTOD!!**")
+    await event.edit("**JANGAN SOK JAGOAN DAH LU**")
     sleep(1)
-    await typew.edit("**JANGAN SOK JAGOAN DAH LU**")
+    await event.edit("**MUKA MASIH KAYA KONTOL AJA**")
     sleep(1)
-    await typew.edit("**MUKA MASIH KAYA KONTOL AJA**")
+    await event.edit("**BANGGA LU HAHAHAHA**")
     sleep(1)
-    await typew.edit("**BANGGA LU HAHAHAHA**")
+    await event.edit("**COBA DEH NGACA MUKA LU KAN HINA BANGET**")
     sleep(1)
-    await typew.edit("**COBA DEH NGACA MUKA LU KAN HINA BANGET**")
+    await event.edit("**HAHAHAHA**")
     sleep(1)
-    await typew.edit("**HAHAHAHA**")
+    await event.edit("**MAKANYA GANTENG KONTOL**")
     sleep(1)
-    await typew.edit("**MAKANYA GANTENG KONTOL**")
+    await event.edit("**BIAR MUKALU GAK DIHINA TERUS**")
     sleep(1)
-    await typew.edit("**BIAR MUKALU GAK DIHINA TERUS**")
+    await event.edit("**SAMA ORANG LAIN**")
     sleep(1)
-    await typew.edit("**SAMA ORANG LAIN**")
-    sleep(1)
-    await typew.edit("**HAHAHAHA**")
+    await event.edit("**HAHAHAHA**")
 # Create by myself @localheart
 
 
-@register(outgoing=True, pattern='^$goblok(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"goblok(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**WOYY GOBLOK!!**")
     sleep(1)
-    await typew.edit("**WOYY GOBLOK!!**")
+    await event.edit("**KO LU GOBLOK BANGET SIH**")
     sleep(1)
-    await typew.edit("**KO LU GOBLOK BANGET SIH**")
+    await event.edit("**OTAK LU TUH KAYA KONTOL**")
     sleep(1)
-    await typew.edit("**OTAK LU TUH KAYA KONTOL**")
+    await event.edit("**YANG LEMBEK KETIKA LEMAH**")
     sleep(1)
-    await typew.edit("**YANG LEMBEK KETIKA LEMAH**")
+    await event.edit("**DAN KERAS KETIKA LU SANGE GOBLOK**")
     sleep(1)
-    await typew.edit("**DAN KERAS KETIKA LU SANGE GOBLOK**")
+    await event.edit("**HAHAHAHA**")
     sleep(1)
-    await typew.edit("**HAHAHAHA**")
+    await event.edit("**MAKANYA JANGAN SANGEAN MULU**")
     sleep(1)
-    await typew.edit("**MAKANYA JANGAN SANGEAN MULU**")
+    await event.edit("**MUKA LU AJA KAYA ASPAL JALANAN**")
     sleep(1)
-    await typew.edit("**MUKA LU AJA KAYA ASPAL JALANAN**")
+    await event.edit("**EHHH SANGE NYA MAU DAPAT YANG CANTIK**")
     sleep(1)
-    await typew.edit("**EHHH SANGE NYA MAU DAPAT YANG CANTIK**")
-    sleep(1)
-    await typew.edit("**HAHAHAHA**")
+    await event.edit("**HAHAHAHA**")
 # Create by myself @localheart
 
 
-@register(outgoing=True, pattern='^$ngatain(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"ngatain(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**BABI!!**")
     sleep(1)
-    await typew.edit("**BABI!!**")
+    await event.edit("**MUKA LU KAYA BABI**")
     sleep(1)
-    await typew.edit("**MUKA LU KAYA BABI**")
+    await event.edit("**OTAK LU TUH KAYA KONTOL**")
     sleep(1)
-    await typew.edit("**OTAK LU TUH KAYA KONTOL**")
+    await event.edit("**MUKA LU HINA BANGET**")
     sleep(1)
-    await typew.edit("**MUKA LU HINA BANGET**")
+    await event.edit("**OTAK LU KAYA BATU**")
     sleep(1)
-    await typew.edit("**OTAK LU KAYA BATU**")
+    await event.edit("**HAHAHAHA**")
     sleep(1)
-    await typew.edit("**HAHAHAHA**")
+    await event.edit("**MAKANYA JANGAN SANGEAN MULU**")
     sleep(1)
-    await typew.edit("**MAKANYA JANGAN SANGEAN MULU**")
+    await event.edit("**KONTOL LU AJA MASIH BENGKOK**")
     sleep(1)
-    await typew.edit("**KONTOL LU AJA MASIH BENGKOK**")
+    await event.edit("**EHHH SANGE NYA MAU DAPAT YANG CANTIK**")
     sleep(1)
-    await typew.edit("**EHHH SANGE NYA MAU DAPAT YANG CANTIK**")
-    sleep(1)
-    await typew.edit("**HAHAHAHA**")
+    await event.edit("**HAHAHAHA**")
 # Create by myself @localheart
 
 
-@register(outgoing=True, pattern='^$yatim(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"yatim(?: |$)(.*)"))
+async def _(event):
+    await event.edit("`Hai Anak Kontol 🙈, Jangan Lupa Makan Yaa`")
     sleep(1)
-    await typew.edit("`Hai Anak Kontol 🙈, Jangan Lupa Makan Yaa`")
+    await event.edit("`Jangan Bilang Lu Ga Dikasih Makan Sama Ortu 😁`")
     sleep(1)
-    await typew.edit("`Jangan Bilang Lu Ga Dikasih Makan Sama Ortu 😁`")
+    await event.edit("`APA PERLU GUA SANTUNIN ?? 🙈🙈 xixixi`")
     sleep(1)
-    await typew.edit("`APA PERLU GUA SANTUNIN ?? 🙈🙈 xixixi`")
+    await event.edit("`OH IYAA LUPAAA, LU KAN BEBAN KELUARGA 🤣`")
     sleep(1)
-    await typew.edit("`OH IYAA LUPAAA, LU KAN BEBAN KELUARGA 🤣`")
+    await event.edit("`MANA MUNGKIN ORTU LU PEDULII xixixi 🙈`")
     sleep(1)
-    await typew.edit("`MANA MUNGKIN ORTU LU PEDULII xixixi 🙈`")
+    await event.edit("`KETAWA DULU BOLEH KALI YAA 😁`")
     sleep(1)
-    await typew.edit("`KETAWA DULU BOLEH KALI YAA 😁`")
+    await event.edit("`HAHAHAHAHAHAHA`")
     sleep(1)
-    await typew.edit("`HAHAHAHAHAHAHA`")
+    await event.edit("`KASIAN ORTUNYAA GAPEDULIII 🙈🤣`")
     sleep(1)
-    await typew.edit("`KASIAN ORTUNYAA GAPEDULIII 🙈🤣`")
+    await event.edit("`MAAF YA, CANDAA BEBANNNN xixixi 🙈`")
     sleep(1)
-    await typew.edit("`MAAF YA, CANDAA BEBANNNN xixixi 🙈`")
-    sleep(1)
-    await typew.edit("`Tapi Bo'ong Hiyahiyahiya`")
+    await event.edit("`Tapi Bo'ong Hiyahiyahiya`")
 # Create by myself @localheart
 
 
-@register(outgoing=True, pattern=r"^\$kont(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    sleep(1)
-    await typew.edit("**KONTOLL**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"kont(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**KONTOLL**")
     sleep(1.5)
-    await typew.edit("**LU ANAK KONTOLL**")
+    await event.edit("**LU ANAK KONTOLL**")
     sleep(1.5)
-    await typew.edit("**DI BIKIN DARI KONTOLL**")
+    await event.edit("**DI BIKIN DARI KONTOLL**")
     sleep(1.5)
-    await typew.edit("**MUKALU PERSIS KONTOLL**")
+    await event.edit("**MUKALU PERSIS KONTOLL**")
     sleep(1.5)
-    await typew.edit("**DASAR ANAK NGONTOLLLL**")
+    await event.edit("**DASAR ANAK NGONTOLLLL**")
     sleep(1.5)
-    await typew.edit("**NOLEP KONTOLL**")
+    await event.edit("**NOLEP KONTOLL**")
     sleep(1.5)
-    await typew.edit("**NGERUSUH KONTOLL**")
+    await event.edit("**NGERUSUH KONTOLL**")
     sleep(1.5)
-    await typew.edit("**BENER BENER KONTOLL**")
+    await event.edit("**BENER BENER KONTOLL**")
     sleep(1.5)
-    await typew.edit("**PADAHAL LO GAPUNYA KONTOLL**")
+    await event.edit("**PADAHAL LO GAPUNYA KONTOLL**")
     sleep(1.5)
-    await typew.edit("**MENDING LO OPERASI KONTOLL**")
+    await event.edit("**MENDING LO OPERASI KONTOLL**")
     sleep(1.5)
-    await typew.edit("**BIAR LO PUNYA KONTOLL**")
+    await event.edit("**BIAR LO PUNYA KONTOLL**")
     sleep(1.5)
-    await typew.edit("**KASIAN CACAD GAPUNYA KONTOLL**")
+    await event.edit("**KASIAN CACAD GAPUNYA KONTOLL**")
 
+CMD_HELP.update(
+    {
+        "yinstoxic2": f"**Plugin : **`yinstoxic2`\
+        \n\n  •  **Syntax :** `{cmd}ngentot`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}goblok`\
+        \n  •  **Function : **Cobaij sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}ngatain`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}kont`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}yatim`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n**Klo mau Req, kosa kata dari lu Bisa pake Module costum. Ketik** `{cmd}help costum`\
+    "
+    }
+) 
 
-CMD_HELP.update({
-    "yinstoxic2": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `$ngentot`\
-    \n↳ : Cobain aja\
-    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `$goblok`\
-    \n↳ : Cobain aja\
-    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `$ngatain`\
-    \n↳ : Cobain aja\
-    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `$kont`\
-    \n↳ : Cobain aja\
-    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `$yatim`\
-    \n↳ : Cobain aja."
-})
