093c522f977e157aebe6b109bcd301fb69a2e4df
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-02-13 16:56:17 +0800

    Update yinsubot.py

--- userbot/modules/yinsubot.py
+++ userbot/modules/yinsubot.py
@@ -4,194 +4,179 @@
 
 
 from time import sleep
-from userbot import CMD_HELP
-from userbot.events import register
+
+from userbot import BLACKLIST_CHAT
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP, bot
+from userbot.events import ayiin_cmd
 
 
-@register(outgoing=True, pattern="^$sadboy(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
+
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"sadboy(?: |$)(.*)"))
+async def _(event):
+    await event.edit("`Pertama-tama kamu cantik`")
     sleep(2)
-    await typew.edit("`Pertama-tama kamu cantik`")
-    sleep(2)
-    await typew.edit("`Kedua kamu manis`")
+    await event.edit("`Kedua kamu manis`")
     sleep(1)
-    await typew.edit("`Dan yang terakhir adalah kamu bukan milikku`")
+    await event.edit("`Dan yang terakhir adalah kamu bukan milikku`")
 
 
 # Create by myself @localheart
 
 
-@register(outgoing=True, pattern="^$lahk(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("`Lahk, Lo tolol?`")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"lah(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**Lah, Lu tolol?**")
     sleep(1)
-    await typew.edit("`Apa dongok?`")
+    await event.edit("**Apa dongok?**")
     sleep(1)
-    await typew.edit("`Gausah sok keras`")
+    await event.edit("**Gausah sok keras**")
     sleep(1)
-    await typew.edit("`Gua ga ketrigger sama bocah baru nyemplung!`")
+    await event.edit("**Gua ga ketrigger sama bocah baru nyemplung!**")
 
 
-@register(outgoing=True, pattern=r"^\$sok(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    sleep(1.5)
-    await typew.edit("**WOII**")
-    sleep(1.5)
-    await typew.edit("**KONTOL**")
-    sleep(1.5)
-    await typew.edit("**KALO MENTAL MASIH PATUNGAN**")
-    sleep(1.5)
-    await typew.edit("**GAUSAH SOK KERAS DEH**")
-    sleep(1.5)
-    await typew.edit("**GA KEREN LO BEGITU NGENTOT**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"sok(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**WOII**")
+    sleep(2)
+    await event.edit("**KONTOL**")
+    sleep(2)
+    await event.edit("**KALO MENTAL MASIH PATUNGAN**")
+    sleep(2)
+    await event.edit("**GAUSAH SOK KERAS DEH**")
+    sleep(2)
+    await event.edit("**GA KEREN LO BEGITU NGENTOT**")
 
 
-@register(outgoing=True, pattern="^$wah(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("`Wahh, War nya keren bang`")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"wah(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**Wahh, War nya keren bang**")
     sleep(2)
-    await typew.edit("`Tapi, Yang gua liat, kok Kaya lawakan`")
+    await event.edit("**Tapi, Yang gua liat, kok Kaya lawakan**")
     sleep(2)
-    await typew.edit("`Oh iya, Kan lo badut 🤡`")
+    await event.edit("**Oh iya, Kan lo badut 🤡**")
     sleep(2)
-    await typew.edit("`Kosa kata pas ngelawak, Jangan di pake war bang`")
+    await event.edit("**Kosa kata pas ngelawak, Jangan di pake war bang**")
     sleep(2)
-    await typew.edit("`Kesannya lo ngasih kita hiburan.`")
+    await event.edit("**Kesannya lo ngasih kita hiburan.**")
     sleep(2)
-    await typew.edit("`Kasian badut🤡, Ga di hargain pengunjung, Eh lampiaskan nya ke Tele, Wkwkwk`")
+    await event.edit("**Kasian badut🤡, Ga di hargain pengunjung, Eh lampiaskan nya ke Tele, Wkwkwk**")
     sleep(3)
-    await typew.edit("`Dah sana cabut, Makasih hiburannya, Udah bikin Gua tawa ngakak`")
+    await event.edit("**Dah sana cabut, Makasih hiburannya, Udah bikin Gua tawa ngakak**")
 
 
-@register(outgoing=True, pattern="^$alay(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    sleep(1)
-    await typew.edit("eh kamu, iya kamu")
-    sleep(1)
-    await typew.edit("**ALAY** bnget sih")
-    sleep(1)
-    await typew.edit("spam bot mulu")
-    sleep(1)
-    await typew.edit("baru jadi userbot ya?? xixixi")
-    sleep(1)
-    await typew.edit("pantes **NORAK**")
-
-
-@register(outgoing=True, pattern="^$erpe(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    sleep(1)
-    await typew.edit("Hai, Kamu Anak Erpe Ya")
-    sleep(1)
-    await typew.edit("Kok Pake Muka Orang sih?")
-    sleep(1)
-    await typew.edit("Oh iya, Muka Anak Erpe Kan")
-    sleep(1)
-    await typew.edit("**BURIK - BURIK**")
-    sleep(1)
-    await typew.edit("Jadinya Pake Muka Orang")
-    sleep(1)
-    await typew.edit("Karena Muka Anak erpe")
-    sleep(1)
-    await typew.edit("**BURIK - BURIK**")
-    sleep(1)
-    await typew.edit("Canda **BURIK**")
-    sleep(1)
-    await typew.edit("Lari Ada Plastik KePanasan")
-
-
-@register(outgoing=True, pattern="^$ange(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("`Ayanggggg😖`")
-    sleep(1)
-    await typew.edit("`Ange😫`")
-    sleep(1)
-    await typew.edit("`Ayukkk Ewean Ayanggg🤤`")
-
-
-@register(outgoing=True, pattern=r"^\$virtual(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**OOOO**")
-    sleep(1.5)
-    await typew.edit("**INI YANG VIRTUAL**")
-    sleep(1.5)
-    await typew.edit("**YANG KATANYA SAYANG BANGET**")
-    sleep(1.5)
-    await typew.edit("**TAPI TETEP AJA DI TINGGAL**")
-    sleep(1.5)
-    await typew.edit("**NI INGET**")
-    sleep(1.5)
-    await typew.edit("**TANGANNYA AJA GA BISA DI PEGANG**")
-    sleep(1.5)
-    await typew.edit("**APALAGI OMONGANNYA**")
-    sleep(1.5)
-    await typew.edit("**BHAHAHAHA**")
-    sleep(1.5)
-    await typew.edit("**KASIAN MANA MASIH MUDA**")
-
-
-@register(outgoing=True, pattern="^$tittle(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**OI ANAK TITLE**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"alay(?: |$)(.*)"))
+async def _(event):
+    await event.edit("eh kamu, iya kamu")
     sleep(2)
-    await typew.edit("**OOO INI YANG SOK JADI PAHLAWAN DI TELEGRAM?**")
+    await event.edit("**ALAY bnget sih**")
+    sleep(2)
+    await event.edit("**spam bot mulu**")
+    sleep(2)
+    await event.edit("**baru jadi userbot ya?? xixixi**")
+    sleep(2)
+    await event.edit("**pantes NORAK**")
+
+
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"erpe(?: |$)(.*)"))
+async def _(event):
+    await event.edit("Hai, Kamu Anak Erpe Ya")
+    sleep(1)
+    await event.edit("Kok Pake Muka Orang sih?")
+    sleep(1)
+    await event.edit("Oh iya, Muka Anak Erpe Kan")
+    sleep(1)
+    await event.edit("**BURIK - BURIK**")
+    sleep(1)
+    await event.edit("Jadinya Pake Muka Orang")
+    sleep(1)
+    await event.edit("Karena Muka Anak erpe")
+    sleep(1)
+    await event.edit("**BURIK - BURIK**")
+    sleep(1)
+    await event.edit("Canda **BURIK**")
+    sleep(1)
+    await event.edit("Lari Ada Plastik KePanasan")
+
+
+
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"virtual(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**OOOO**")
+    sleep(1.5)
+    await event.edit("**INI YANG VIRTUAL**")
+    sleep(1.5)
+    await event.edit("**YANG KATANYA SAYANG BANGET**")
+    sleep(1.5)
+    await event.edit("**TAPI TETEP AJA DI TINGGAL**")
+    sleep(1.5)
+    await event.edit("**NI INGET**")
+    sleep(1.5)
+    await event.edit("**TANGANNYA AJA GA BISA DI PEGANG**")
+    sleep(1.5)
+    await event.edit("**APALAGI OMONGANNYA**")
+    sleep(1.5)
+    await event.edit("**BHAHAHAHA**")
+    sleep(1.5)
+    await event.edit("**KASIAN MANA MASIH MUDA**")
+
+
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"tittle(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**OI ANAK TITLE**")
+    sleep(2)
+    await event.edit("**OOO INI YANG SOK JADI PAHLAWAN DI TELEGRAM?**")
     sleep(3)
-    await typew.edit("**TITLE KEMANA MANA SAMPE MENUHIN NAMA**")
+    await event.edit("**TITLE KEMANA MANA SAMPE MENUHIN NAMA**")
     sleep(2)
-    await typew.edit("**ADA YANG SAMPE 18+ LAH SEGALA MACEM**")
+    await event.edit("**ADA YANG SAMPE 18+ LAH SEGALA MACEM**")
     sleep(2)
-    await typew.edit("**LO KIRA KEREN KEK GITU?**")
+    await event.edit("**LO KIRA KEREN KEK GITU?**")
     sleep(2)
-    await typew.edit("**KERJAAN CUMA NGURUSIN GRUP DI TELEGRAM SAMA NGAJAK ORANG WAR**")
+    await event.edit("**KERJAAN CUMA NGURUSIN GRUP DI TELEGRAM SAMA NGAJAK ORANG WAR**")
     sleep(4)
-    await typew.edit("**YAELAH BRO MENTAL LO CUMA DI SOSMED APA GIMANE?**")
+    await event.edit("**YAELAH BRO MENTAL LO CUMA DI SOSMED APA GIMANE?**")
     sleep(2)
-    await typew.edit("**PERASAAN DULU TELEGRAM GAADA DEH BOCAH BOCAH SOK JAGO KEK GINI**")
+    await event.edit("**PERASAAN DULU TELEGRAM GAADA DEH BOCAH BOCAH SOK JAGO KEK GINI**")
     sleep(2)
-    await typew.edit("**GILIRAN TITLE NYA DI EJEK NGADU KE OWNER NYA**")
+    await event.edit("**GILIRAN TITLE NYA DI EJEK NGADU KE OWNER NYA**")
     sleep(4)
-    await typew.edit("**TRUS NGAJAK WAR**")
+    await event.edit("**TRUS NGAJAK WAR**")
     sleep(2)
-    await typew.edit("**BUSET DAH BANG**")
+    await event.edit("**BUSET DAH BANG**")
     sleep(2)
-    await typew.edit("**UDAH SEJAGO APESI SAMPE GC DIBELA BELA**")
+    await event.edit("**UDAH SEJAGO APESI SAMPE GC DIBELA BELA**")
     sleep(3)
-    await typew.edit("**ORANG TUA LO NOH ADA YANG NAGIH UTANG UDA LO BELA BELOM?**")
+    await event.edit("**ORANG TUA LO NOH ADA YANG NAGIH UTANG UDA LO BELA BELOM?**")
     sleep(4)
-    await typew.edit("**RELA NGUTANG DEMI NGIDUPIN LU**")
+    await event.edit("**RELA NGUTANG DEMI NGIDUPIN LU**")
     sleep(2)
-    await typew.edit("**EH ANAKNYA MALAH NGEBELAIN GC GAJELAS HAHAHA**")
+    await event.edit("**EH ANAKNYA MALAH NGEBELAIN GC GAJELAS HAHAHA**")
     sleep(3)
-    await typew.edit("**MANA VIRTUAL LAGI, SOK JAGO LAGI DUH**")
+    await event.edit("**MANA VIRTUAL LAGI, SOK JAGO LAGI DUH**")
     sleep(3)
-    await typew.edit("**SEMOGA CEPET SADAR YA HAHAHAHA**")
+    await event.edit("**SEMOGA CEPET SADAR YA HAHAHAHA**")
 
-
-CMD_HELP.update({
-    "yinsubot": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `$sadboy`\
-    \n↳ : Biasalah sadboy hikss\
-    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `$wah`\
-    \n↳ : Ngatain orang war\
-    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `$sok`\
-    \n↳ : Ngatain orang sok keras2\
-    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `$lahk`\
-    \n↳ : Ngatain orang sok keras.\
-    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `$alay`\
-    \n↳ : Ngatain orang alay\
-    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `$erpe`\
-    \n↳ : Ngatain anak erpe\
-    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `$ange`\
-    \n↳ : Sangean\
-    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `$virtual`\
-    \n↳ : Ngatain bocah virtual.\
-    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `$tittle`\
-    \n↳ : Ngatain bocah gila tittle."
-})
+CMD_HELP.update(
+    {
+        "yinsubot": f"**Plugin : **`yinsubot`\
+        \n\n  •  **Syntax :** `{cmd}sadboy`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}wah`\
+        \n  •  **Function : **Cobaij sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}sok`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}lah`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}alay`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}erpe`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}tittle`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** '{cmd}virtual'\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n**Klo mau Req, kosa kata dari lu Bisa pake Module costum. Ketik** `{cmd}help costum`\
+    "
+    }
+) 
