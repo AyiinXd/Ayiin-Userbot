c07e4bf156256b308aa470bcdb04223b7032fe7d
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-02-13 17:42:56 +0800

    Update yinstoxic.py

--- userbot/modules/yinstoxic.py
+++ userbot/modules/yinstoxic.py
@@ -1,197 +1,176 @@
-from platform import uname
+from time import sleep
+
+from userbot import BLACKLIST_CHAT
 from userbot import CMD_HANDLER as cmd
-from userbot import ALIVE_NAME, CMD_HELP
-from userbot.events import register
-
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-# ============================================
+from userbot import CMD_HELP, bot
+from userbot.events import ayiin_cmd
 
 
-@register(outgoing=True, pattern='^$d(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**BACOT NGENTOTTT!!BAPA LU SURUH RIBUT SAMA GUA**")
+
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"d(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**BACOT NGENTOTTT!!BAPA LU SURUH RIBUT SAMA GUA**")
 
 
-@register(outgoing=True, pattern='^$e(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**GAK USAH SOK KERAS GOBLOK!!KENCING MASIH BERDIRI AJA BELAGU**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"e(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**GAK USAH SOK KERAS GOBLOK!!KENCING MASIH BERDIRI AJA BELAGU**")
 
 
-@register(outgoing=True, pattern='^$f(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**MUKA LU SEMUA KAYA KONTOL HAHAHAHA!!**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"f(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**MUKA LU SEMUA KAYA KONTOL HAHAHAHA!!**")
 
 
-@register(outgoing=True, pattern='^$i(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**KONTOL MASIH BENGKOK AJA BANGGA LU HAHAHAHA!!**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"i(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**KONTOL MASIH BENGKOK AJA BANGGA LU HAHAHAHA!!**")
 
 
-@register(outgoing=True, pattern='^$r(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**KONTOL KONTOL APA YANG BESAR?KONTOL LU LAH HAHAHAHA!!**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"r(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**KONTOL KONTOL APA YANG BESAR?KONTOL LU LAH HAHAHAHA!!**")
 
 
-@register(outgoing=True, pattern='^$t(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**BABI!!KONTOL!!NGENTOT!!!**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"t(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**BABI!!KONTOL!!NGENTOT!!!**")
 
 
-@register(outgoing=True, pattern='^$u(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**BABI LU GOBLOK!!GANTENGAN JUGA GUA HAHAHAHA**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"u(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**BABI LU GOBLOK!!GANTENGAN JUGA GUA HAHAHAHA**")
 
 
-@register(outgoing=True, pattern='^$w(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**BABI LU GOBLOK!!CANTIKAN JUGA GUA HAHAHAHA**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"w(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**BABI LU GOBLOK!!CANTIKAN JUGA GUA HAHAHAHA**")
 
 
-@register(outgoing=True, pattern='^$z(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**BACOTAN LU GAK BIKIN GUA TREMOR GOBLOK HAHAHAHA!!**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"bct(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**BACOTAN LU GAK BIKIN GUA TREMOR GOBLOK HAHAHAHA!!**")
 
 
-@register(outgoing=True, pattern='^$k(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**HAI PERKENALKAN NAMA SAYA GAK TAU LUPA!!**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"k(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**HAI PERKENALKAN NAMA SAYA GAK TAU LUPA!!**")
 
 
-@register(outgoing=True, pattern='^$n(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**GIMANA KABAR KAMU SAYANG??APAKAH BAIK?**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"n(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**GIMANA KABAR KAMU SAYANG??APAKAH BAIK?**")
 
 
-@register(outgoing=True, pattern='^$b(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**ELEEEHHHH SOK BANGET KEPINTERAN KAMU!!**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"b(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**ELEEEHHHH SOK BANGET KEPINTERAN KAMU!!**")
 
 
-@register(outgoing=True, pattern='^$m(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**INI GRUB APA KUBURAN SEPI BANGET ASTAGFIRULLAH!!**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"m(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**INI GRUB APA KUBURAN SEPI BANGET ASTAGFIRULLAH!!**")
 
 
-@register(outgoing=True, pattern='^$c(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**KAN UDAH GUA BILANG??MAKANYA JANGAN NGEYEL!!**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"c(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**KAN UDAH GUA BILANG??MAKANYA JANGAN NGEYEL!!**")
 
 
-@register(outgoing=True, pattern='^$s(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**GAUSAH SOKAP DEH KAMU!!**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"s(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**GAUSAH SOKAP DEH KAMU!!**")
 
 
-@register(outgoing=True, pattern='^$v(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**MACAM BAGUS AE LU BEGITU HMM!!**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"v(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**MACAM BAGUS AE LU BEGITU HMM!!**")
 
 
-@register(outgoing=True, pattern='^$j(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**MAAF BUKAN JAGOAN HAHAHAHA!!**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"j(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**MAAF BUKAN JAGOAN HAHAHAHA!!**")
 
 
-@register(outgoing=True, pattern='^$a(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**BISMILLAH SLEEP CALL!!😁**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"z(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**BISMILLAH SLEEP CALL!!😁**")
 
 
-@register(outgoing=True, pattern='^$g(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**GANTENG LU BEGITU???**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"g(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**GANTENG LU BEGITU???**")
 
 
-@register(outgoing=True, pattern='^$y(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**War War Tai anjing, Ketrigger minta sharelok, Udah di sharelok Ga nyamperin,Keras di sosmed Bhakss...**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"y(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**War War Tai anjing, Ketrigger minta sharelok, Udah di sharelok Ga nyamperin,Keras di sosmed Bhakss...**")
 
 
-@register(outgoing=True, pattern='^$h(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**CANTIK LU BEGITU???**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"h(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**CANTIK LU BEGITU???**")
 
 
-@register(outgoing=True, pattern='^$o(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**MENTANG MENTANG PUNYA BOT MAINNYA BOT!!PANTES MUKANYA KAYA BOT😁**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"o(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**MENTANG MENTANG PUNYA BOT MAINNYA BOT!!PANTES MUKANYA KAYA BOT😁**")
 
 
-@register(outgoing=True, pattern='^$a(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("**NORAK LU KONTOL!! MAKE USERBOT CUMAN BUAT WAR MALAH SOK KERAS**")
+@bot.on(ayiin_cmd(outgoing=True, pattern=r"a(?: |$)(.*)"))
+async def _(event):
+    await event.edit("**NORAK LU KONTOL!! MAKE USERBOT CUMAN BUAT WAR MALAH SOK KERAS**")
 
-
-CMD_HELP.update({
-    "yinstoxic":
-    "$d\
-\nUsage: Bacotin Orang.\
-\n\n$e\
-\nUsage: Buat Orang Yang Sok Keras.\
-\n\n$f\
-\nUsage: Ngatain Orang Wkwkkw.\
-\n\n$i\
-\nUsage: Kontol Orang Ngatain.\
-\n\n$r\
-\nUsage: Pantun Anjing.\
-\n\n$t\
-\nUsage: Nyebutin Binatang.\
-\n\n$u\
-\nUsage: Biar Dikata Ganteng.\
-\n\n$w\
-\nUsage: Biar Dikata Cantik.\
-\n\n$z\
-\nUsage: Tremor Kan Lu.\
-\n\n.k\
-\nUsage: Memperkenalkan Diri.\
-\n\n$n\
-\nUsage: Menanyakan Kabar.\
-\n\n$b\
-\nUsage: Sok Kepinteran.\
-\n\n$m\
-\nUsage: Gc Nya Kaya kuburan.\
-\n\n$c\
-\nUsage: Dia tuh Ngeyel banget.\
-\n\n$s\
-\nUsage: Haha sokap.\
-\n\n$v\
-\nUsage: Merendah.\
-\n\n$a\
-\nUsage: Nyari Sleep Call.\
-\n\n$j\
-\nUsage: Hujat yang gapunya muka.\
-\n\n$g\
-\nUsage: Kegantengan.\
-\n\n$y\
-\nUsage: teruntuk petarung.\
-\n\n$h\
-\nUsage: Kecantikan.\
-\n\n$o\
-\nUsage: Ngatain org norak.\
-\n\n$a\
-\nUsage: Ngatain Petarunx Tele."
-})
+CMD_HELP.update(
+    {
+        "yinstoxic": f"**Plugin : **`yinstoxic`\
+        \n\n  •  **Syntax :** `{cmd}d`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}e`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}f`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}i`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}r`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}t`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}u`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}w`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}z`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}k`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}n`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}b`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}m`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}c`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}s`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}v`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}a`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}j`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}g`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}y`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}h`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}o`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n  •  **Syntax :** `{cmd}bct`\
+        \n  •  **Function : **Cobain sendiri tod\
+        \n\n**Klo mau Req, kosa kata dari lu Bisa pake Module costum. Ketik** `{cmd}help costum`\
+    "
+    }
+)
