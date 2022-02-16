af1bbb41b46b198ce673c7935ce92d8752e8ebba
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-02-16 10:33:14 +0800

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -37,5 +37,8 @@ async def help(event):
             f"**߷ 𝙹𝚄𝙼𝙻𝙰𝙷** `{len(modules)}` **Modules**\n"
             f"**♕︎ 𝙾𝚆𝙽𝙴𝚁:** [{user.first_name}](tg://user?id={user.id})\n\n"
             f"{ICON_HELP}   {string}"
-            f"\n\n✐ **𝙽𝙾𝚃𝙴𝚂 :**  `$help yinsubot`\n☞  𝚂𝚄𝙿𝙿𝙾𝚁𝚃 : @AyiinXdSupport"
+            f"\n\n𝚂𝚄𝙿𝙿𝙾𝚁𝚃 : @{CHANNEL}",
+        )
+        await event.reply(
+            f"\n**Contoh Ketik** `{cmd}help yinsubot` **Untuk Melihat Informasi Module**"
         )
