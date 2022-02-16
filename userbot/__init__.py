c0c525725ad9ea00f53cc1f455b0bb6d08c71e01
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-02-17 06:27:41 +0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -492,7 +492,7 @@ with bot:
             builder = event.builder
             result = None
             query = event.text
-            if event.query.user_id == uid and query.startswith("@SharingUserbot"):
+            if event.query.user_id == uid and query.startswith("@AyiinXdSupport"):
                 buttons = paginate_help(0, dugmeler, "helpme")
                 result = builder.photo(
                     file=logoman,
