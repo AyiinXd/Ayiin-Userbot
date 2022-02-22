116d476ce1f8e299601f807bd3f72dd954b16c42
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-02-23 05:56:36 +0800

    Update yinsubot3.py

--- userbot/modules/yinsubot3.py
+++ userbot/modules/yinsubot3.py
@@ -7,48 +7,48 @@ from time import sleep
 
 from userbot import BLACKLIST_CHAT
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, bot
-from userbot.events import ayiin_cmd
+from userbot import CMD_HELP
+from userbot.utils import edit_or_reply, ayiin_cmd
 
 
-@bot.on(ayiin_cmd(outgoing=True, pattern=r"yins(?: |$)(.*)"))
+@ayiin_cmd(pattern=r"yins(?: |$)(.*)")
 async def _(event):
-    await event.edit("𝙃𝙖𝙞 𝙋𝙚𝙧𝙠𝙚𝙣𝙖𝙡𝙠𝙖𝙣 𝙉𝙖𝙢𝙖 𝙂𝙪𝙖 𝘼𝙮𝙞𝙞𝙣")
+    ayiin = await edit_or_reply(event, "𝙃𝙖𝙞 𝙋𝙚𝙧𝙠𝙚𝙣𝙖𝙡𝙠𝙖𝙣 𝙉𝙖𝙢𝙖 𝙂𝙪𝙖 𝘼𝙮𝙞𝙞𝙣")
     sleep(3)
-    await event.edit("23 𝙏𝙖𝙝𝙪𝙣")
+    await ayiin.edit("23 𝙏𝙖𝙝𝙪𝙣")
     sleep(2)
-    await event.edit("𝙏𝙞𝙣𝙜𝙜𝙖𝙡 𝘿𝙞 𝘽𝙖𝙡𝙞...")
+    await ayiin.edit("𝙏𝙞𝙣𝙜𝙜𝙖𝙡 𝘿𝙞 𝘽𝙖𝙡𝙞...")
     sleep(3)
-    await event.edit("𝙊𝙬𝙣𝙚𝙧 𝘿𝙖𝙧𝙞 𝘼𝙮𝙞𝙞𝙣-𝙐𝙨𝙚𝙧𝙗𝙤𝙩, 𝙎𝙖𝙡𝙖𝙢 𝙆𝙚𝙣𝙖𝙡 😁")
+    await ayiin.edit("𝙊𝙬𝙣𝙚𝙧 𝘿𝙖𝙧𝙞 𝘼𝙮𝙞𝙞𝙣-𝙐𝙨𝙚𝙧𝙗𝙤𝙩, 𝙎𝙖𝙡𝙖𝙢 𝙆𝙚𝙣𝙖𝙡 😁")
 # Create by myself @AyiinXd
 
 
-@bot.on(ayiin_cmd(outgoing=True, pattern=r"sayang(?: |$)(.*)"))
+@ayiin_cmd(pattern=r"sayang(?: |$)(.*)")
 async def _(event):
-    await event.edit("𝘼𝙠𝙪 𝘾𝙪𝙢𝙖 𝙈𝙖𝙪 𝘽𝙞𝙡𝙖𝙣𝙜 👉👈")
+    ayiin = await edit_or_reply(event, "𝘼𝙠𝙪 𝘾𝙪𝙢𝙖 𝙈𝙖𝙪 𝘽𝙞𝙡𝙖𝙣𝙜 👉👈")
     sleep(3)
-    await event.edit("𝘼𝙠𝙪 𝙎𝙖𝙮𝙖𝙣𝙜 𝙆𝙖𝙢𝙪 😘")
+    await ayiin.edit("𝘼𝙠𝙪 𝙎𝙖𝙮𝙖𝙣𝙜 𝙆𝙖𝙢𝙪 😘")
     sleep(1)
-    await event.edit("𝙈𝙪𝙖𝙖𝙘𝙘𝙝𝙝𝙝 😘💕")
+    await ayiin.edit("𝙈𝙪𝙖𝙖𝙘𝙘𝙝𝙝𝙝 😘💕")
 # Create by myself @AyiinXd
 
 
-@bot.on(ayiin_cmd(outgoing=True, pattern=r"semangat(?: |$)(.*)"))
+@ayiin_cmd(pattern=r"semangat(?: |$)(.*)")
 async def _(event):
-    await event.edit("𝘼𝙥𝙖𝙥𝙪𝙣 𝙔𝙖𝙣𝙜 𝙏𝙚𝙧𝙟𝙖𝙙𝙞")
+    ayiin = await edit_or_reply(event, "𝘼𝙥𝙖𝙥𝙪𝙣 𝙔𝙖𝙣𝙜 𝙏𝙚𝙧𝙟𝙖𝙙𝙞")
     sleep(3)
-    await event.edit("𝙏𝙚𝙩𝙖𝙥𝙡𝙖𝙝 𝘽𝙚𝙧𝙣𝙖𝙥𝙖𝙨")
+    await ayiin.edit("𝙏𝙚𝙩𝙖𝙥𝙡𝙖𝙝 𝘽𝙚𝙧𝙣𝙖𝙥𝙖𝙨")
     sleep(1)
-    await event.edit("𝘿𝙖𝙣 𝙎𝙚𝙡𝙖𝙡𝙪 𝘽𝙚𝙧𝙨𝙮𝙪𝙠𝙪𝙧")
+    await ayiin.edit("𝘿𝙖𝙣 𝙎𝙚𝙡𝙖𝙡𝙪 𝘽𝙚𝙧𝙨𝙮𝙪𝙠𝙪𝙧")
 # Create by myself @AyiinXd
 
-@bot.on(ayiin_cmd(outgoing=True, pattern=r"mengeluh(?: |$)(.*)"))
+@ayiin_cmd(pattern=r"mengeluh(?: |$)(.*)")
 async def _(event):
-    await event.edit("𝘼𝙥𝙖𝙥𝙪𝙣 𝙔𝙖𝙣𝙜 𝙏𝙚𝙧𝙟𝙖𝙙𝙞")
+    ayiin = await edit_or_reply(event, "𝘼𝙥𝙖𝙥𝙪𝙣 𝙔𝙖𝙣𝙜 𝙏𝙚𝙧𝙟𝙖𝙙𝙞")
     sleep(3)
-    await event.edit("𝙏𝙚𝙩𝙖𝙥𝙡𝙖𝙝 𝙈𝙚𝙣𝙜𝙚𝙡𝙪𝙝")
+    await ayiin.edit("𝙏𝙚𝙩𝙖𝙥𝙡𝙖𝙝 𝙈𝙚𝙣𝙜𝙚𝙡𝙪𝙝")
     sleep(1)
-    await event.edit("𝘿𝙖𝙣 𝙎𝙚𝙡𝙖𝙡𝙪 𝙋𝙪𝙩𝙪𝙨 𝘼𝙨𝙖")
+    await ayiin.edit("𝘿𝙖𝙣 𝙎𝙚𝙡𝙖𝙡𝙪 𝙋𝙪𝙩𝙪𝙨 𝘼𝙨𝙖")
 # Create by myself @AyiinXd
 
 
