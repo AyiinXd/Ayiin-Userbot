f6c688e2f259ce8b5d93fe9f35217d91716089e6
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-02-28 22:29:27 +0800

    Update quotly.py

--- userbot/modules/quotly.py
+++ userbot/modules/quotly.py
@@ -78,7 +78,7 @@ async def quotly(event):
         file = await create_quotly(reply_, bg=match, reply=replied_to, sender=user)
     except Exception as er:
         return await msg.edit(str(er))
-    message = await edit_or_reply(event, "Quotly by Ayiin-Userbot")
+    message = await edit_or_reply(event, "Quotly by Ayiin-Userbot", file=file)
     os.remove(file)
     await msg.delete()
     return message
