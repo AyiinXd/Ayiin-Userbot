4889d1be702ce13eecbbe9f854a5214190cd0112
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-02-28 21:47:05 +0800

    Update quotly.py

--- userbot/modules/quotly.py
+++ userbot/modules/quotly.py
@@ -78,7 +78,7 @@ async def quotly(event):
     except Exception as er:
         return await msg.edit(f"**ERROR:** `{er}`")
     message = await reply.reply("Quotly by Ayiin-Userbot", file=file)
-    remove(file)
+    os.remove(file)
     await msg.delete()
     return message
 
