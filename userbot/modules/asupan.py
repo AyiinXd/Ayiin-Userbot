e5aed7a37b5e2741bbc67ae05fdeae29b3b65089
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-03-01 10:54:02 +0800

    Update asupan.py

--- userbot/modules/asupan.py
+++ userbot/modules/asupan.py
@@ -1,41 +1,53 @@
 # 🍀 © @tofik_dn
 # ⚠️ Do not remove credits
 
-import requests
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
 from userbot.utils import ayiin_cmd
+import random
+from userbot import owner
+from telethon.tl.types import InputMessagesFilterVideo
+from telethon.tl.types import InputMessagesFilterVoice
 
 
 @ayiin_cmd(pattern="asupan$")
 async def _(event):
     try:
-        response = requests.get("https://api-tede.herokuapp.com/api/asupan/ptl").json()
-        await event.client.send_file(event.chat_id, response["url"])
+        asupannya = [
+            asupan
+            async for asupan in event.client.iter_messages(
+                "@tedeasupancache", filter=InputMessagesFilterVideo
+            )
+        ]
+        aing = await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(asupannya),
+            caption=f"Nih kak asupannya [{owner}](tg://user?id={aing.id})",
+        )
         await event.delete()
     except Exception:
-        await event.edit("**Tidak bisa menemukan video asupan.**")
+        await event.edit("**Maaf tod tidak bisa menemukan video asupan.**")
 
-
-@ayiin_cmd(pattern="wibu$")
+@ayiin_cmd(pattern="desah$")
 async def _(event):
     try:
-        response = requests.get("https://api-tede.herokuapp.com/api/asupan/wibu").json()
-        await event.client.send_file(event.chat_id, response["url"])
+        desahnya = [
+            desah
+            async for desah in event.client.iter_messages(
+                "@DESAHANFCE", filter=InputMessagesFilterVoice
+            )
+        ]
+        aing = await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(desahnya),
+            caption=f"Nih kak desahannya [{owner}](tg://user?id={aing.id})",
+        )
         await event.delete()
     except Exception:
-        await event.edit("**Tidak bisa menemukan video wibu.**")
-
-
-@ayiin_cmd(pattern="chika$")
-async def _(event):
-    try:
-        response = requests.get("https://api-tede.herokuapp.com/api/chika").json()
-        await event.client.send_file(event.chat_id, response["url"])
-        await event.delete()
-    except Exception:
-        await event.edit("**Tidak bisa menemukan video chikakiku.**")
+        await event.edit("**Maaf tod tidak bisa menemukan desahan.**")
 
 
 CMD_HELP.update(
@@ -43,10 +55,8 @@ CMD_HELP.update(
         "asupan": f"**Plugin : **`asupan`\
         \n\n  •  **Syntax :** `{cmd}asupan`\
         \n  •  **Function : **Untuk Mengirim video asupan secara random.\
-        \n\n  •  **Syntax :** `{cmd}wibu`\
-        \n  •  **Function : **Untuk Mengirim video wibu secara random.\
-        \n\n  •  **Syntax :** `{cmd}chika`\
-        \n  •  **Function : **Untuk Mengirim video chikakiku secara random.\
+        \n\n  •  **Syntax :** `{cmd}desah`\
+        \n  •  **Function : **Untuk Mengirim voice desah secara random.\
     "
     }
 )
