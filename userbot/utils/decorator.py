63fe5925d81549cb4288c1d7e2ed97c7faf9cc3f
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-03-05 05:16:37 +0800

    Update decorator.py

--- userbot/utils/decorator.py
+++ userbot/utils/decorator.py
@@ -131,6 +131,15 @@ def asst_cmd(**args):
     return decorator
 
 
+def chataction(**args):
+    def decorator(func):
+        if bot:
+            bot.add_event_handler(func, events.ChatAction(**args))
+        return func
+
+    return decorator
+
+
 def callback(**args):
     """Assistant's callback decorator"""
 
