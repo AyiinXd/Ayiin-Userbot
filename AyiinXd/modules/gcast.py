ae426356afa1ce6f52f137fd0945be3ba171f752
iamuput <111313150+iamuput@users.noreply.github.com>
2022-10-31 15:58:32 +0700

    Update gcast.py

--- AyiinXd/modules/gcast.py
+++ AyiinXd/modules/gcast.py
@@ -48,6 +48,7 @@ GCAST_BLACKLIST = [
     -1001662510083,  # MutualanDestra
     -1001347414136,  # ArunaMutualan
     -1001572486389,  # PluviaMusicGroup
+    -1001608701614,  # UputtSupport
 ]
 
 Heroku = heroku3.from_key(HEROKU_API_KEY)
