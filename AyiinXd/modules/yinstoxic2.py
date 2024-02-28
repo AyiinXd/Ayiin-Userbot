1acf60d2c9ee4063f0ef9ae94efed855550b6266
iamuput <yogaaasp73@gmail.com>
2024-02-28 16:07:14 +0700

    Create yinstoxic2.py

---
+++ AyiinXd/modules/yinstoxic2.py
@@ -0,0 +1,146 @@
+from time import sleep
+
+from AyiinXd import CMD_HELP
+from AyiinXd.ayiin import ayiin_cmd
+
+from . import cmd
+
+@ayiin_cmd(pattern=r"ngentot(?: |$)(.*)")
+async def _(ayiin):
+    yins = await ayiin.edit("**WOYY NGENTOD!!**")
+    sleep(1)
+    await yins.edit("**JANGAN SOK JAGOAN DAH LU**")
+    sleep(3)
+    await yins.edit("**MUKA MASIH KAYA KONTOL AJA**")
+    sleep(3)
+    await yins.edit("**BANGGA LU HAHAHAHA**")
+    sleep(3)
+    await yins.edit("**COBA DEH NGACA MUKA LU KAN HINA BANGET**")
+    sleep(3)
+    await yins.edit("**HAHAHAHA**")
+    sleep(3)
+    await yins.edit("**MAKANYA GANTENG KONTOL**")
+    sleep(3)
+    await yins.edit("**BIAR MUKALU GAK DIHINA TERUS**")
+    sleep(3)
+    await yins.edit("**SAMA ORANG LAIN**")
+    sleep(3)
+    await yins.edit("**HAHAHAHA**")
+# Create by myself @localheart
+
+
+@ayiin_cmd(pattern=r"goblok(?: |$)(.*)")
+async def _(gblk):
+    Ayiin = await gblk.edit("**WOYY GOBLOK!!**")
+    sleep(3)
+    await Ayiin.edit("**KO LU GOBLOK BANGET SIH**")
+    sleep(3)
+    await Ayiin.edit("**OTAK LU TUH KAYA KONTOL**")
+    sleep(3)
+    await Ayiin.edit("**YANG LEMBEK KETIKA LEMAH**")
+    sleep(3)
+    await Ayiin.edit("**DAN KERAS KETIKA LU SANGE GOBLOK**")
+    sleep(3)
+    await Ayiin.edit("**HAHAHAHA**")
+    sleep(3)
+    await Ayiin.edit("**MAKANYA JANGAN SANGEAN MULU**")
+    sleep(3)
+    await Ayiin.edit("**MUKA LU AJA KAYA ASPAL JALANAN**")
+    sleep(3)
+    await Ayiin.edit("**EHHH SANGE NYA MAU DAPAT YANG CANTIK**")
+    sleep(3)
+    await Ayiin.edit("**HAHAHAHA**")
+# Create by myself @localheart
+
+
+@ayiin_cmd(pattern=r"ngatain(?: |$)(.*)")
+async def _(kntl):
+    kon = await kntl.edit("**BABI!!**")
+    sleep(3)
+    await kon.edit("**MUKA LU KAYA BABI**")
+    sleep(3)
+    await kon.edit("**OTAK LU TUH KAYA KONTOL**")
+    sleep(3)
+    await kon.edit("**MUKA LU HINA BANGET**")
+    sleep(3)
+    await kon.edit("**OTAK LU KAYA BATU**")
+    sleep(3)
+    await kon.edit("**HAHAHAHA**")
+    sleep(3)
+    await kon.edit("**MAKANYA JANGAN SANGEAN MULU**")
+    sleep(3)
+    await kon.edit("**KONTOL LU AJA MASIH BENGKOK**")
+    sleep(3)
+    await kon.edit("**EHHH SANGE NYA MAU DAPAT YANG CANTIK**")
+    sleep(3)
+    await kon.edit("**HAHAHAHA**")
+# Create by myself @localheart
+
+
+@ayiin_cmd(pattern=r"yatim(?: |$)(.*)")
+async def _(ytim):
+    ayiinxd = await ytim.edit("`Hai Anak Kontol 🙈, Jangan Lupa Makan Yaa`")
+    sleep(3)
+    await ayiinxd.edit("`Jangan Bilang Lu Ga Dikasih Makan Sama Ortu 😁`")
+    sleep(3)
+    await ayiinxd.edit("`APA PERLU GUA SANTUNIN ?? 🙈🙈 xixixi`")
+    sleep(3)
+    await ayiinxd.edit("`OH IYAA LUPAAA, LU KAN BEBAN KELUARGA 🤣`")
+    sleep(3)
+    await ayiinxd.edit("`MANA MUNGKIN ORTU LU PEDULII xixixi 🙈`")
+    sleep(3)
+    await ayiinxd.edit("`KETAWA DULU BOLEH KALI YAA 😁`")
+    sleep(3)
+    await ayiinxd.edit("`HAHAHAHAHAHAHA`")
+    sleep(3)
+    await ayiinxd.edit("`KASIAN ORTUNYAA GAPEDULIII 🙈🤣`")
+    sleep(3)
+    await ayiinxd.edit("`MAAF YA, CANDAA BEBANNNN xixixi 🙈`")
+    sleep(3)
+    await ayiinxd.edit("`Tapi Bo'ong Hiyahiyahiya`")
+# Create by myself @localheart
+
+
+@ayiin_cmd(pattern=r"kont(?: |$)(.*)")
+async def _(kontol):
+    yins = await kontol.edit("**WOYY NGENTOD!!**")
+    sleep(3)
+    await yins.edit("**LU ANAK KONTOLL**")
+    sleep(3)
+    await yins.edit("**DI BIKIN DARI KONTOLL**")
+    sleep(3)
+    await yins.edit("**MUKALU PERSIS KONTOLL**")
+    sleep(3)
+    await yins.edit("**DASAR ANAK NGONTOLLLL**")
+    sleep(3)
+    await yins.edit("**NOLEP KONTOLL**")
+    sleep(3)
+    await yins.edit("**NGERUSUH KONTOLL**")
+    sleep(3)
+    await yins.edit("**BENER BENER KONTOLL**")
+    sleep(3)
+    await yins.edit("**PADAHAL LO GAPUNYA KONTOLL**")
+    sleep(3)
+    await yins.edit("**MENDING LO OPERASI KONTOLL**")
+    sleep(3)
+    await yins.edit("**BIAR LO PUNYA KONTOLL**")
+    sleep(3)
+    await yins.edit("**KASIAN CACAD GAPUNYA KONTOLL**")
+
+CMD_HELP.update(
+    {
+        "yinstoxic2": f"**Plugin : **`yinstoxic2`\
+        \n\n  »  **Perintah :** `{cmd}ngentot`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}goblok`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}ngatain`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}kont`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}yatim`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n**Klo mau Req, kosa kata dari lu Bisa pake Module costum. Ketik** `{cmd}help costum`\
+    "
+    }
+                  )
