a9638abbe9616a529a8ec3c2761d58256e7ded48
iamuput <yogaaasp73@gmail.com>
2024-02-28 16:08:07 +0700

    Create yinstoxic.py

---
+++ AyiinXd/modules/yinstoxic.py
@@ -0,0 +1,166 @@
+
+from AyiinXd import CMD_HELP
+from AyiinXd.ayiin import ayiin_cmd
+
+from . import cmd
+
+@ayiin_cmd(pattern="d(?: |$)(.*)")
+async def _(a):
+    await a.edit("**BACOT NGENTOTTT!!BAPA LU SURUH RIBUT SAMA GUA**")
+
+
+@ayiin_cmd(pattern="e(?: |$)(.*)")
+async def _(y):
+    await a.edit("**GAK USAH SOK KERAS GOBLOK!!KENCING MASIH BERDIRI AJA BELAGU**")
+
+
+@ayiin_cmd(pattern="f(?: |$)(.*)")
+async def _(i):
+    await i.edit("**MUKA LU SEMUA KAYA KONTOL HAHAHAHA!!**")
+
+
+@ayiin_cmd(pattern="i(?: |$)(.*)")
+async def _(n):
+    await n.edit("**KONTOL MASIH BENGKOK AJA BANGGA LU HAHAHAHA!!**")
+
+
+@ayiin_cmd(pattern="r(?: |$)(.*)")
+async def _(x):
+    await x.edit("**KONTOL KONTOL APA YANG BESAR?KONTOL LU LAH HAHAHAHA!!**")
+
+
+@ayiin_cmd(pattern="t(?: |$)(.*)")
+async def _(d):
+    await d.edit("**BABI!!KONTOL!!NGENTOT!!!**")
+
+
+@ayiin_cmd(pattern="u(?: |$)(.*)")
+async def _(a):
+    await a.edit("**BABI LU GOBLOK!!GANTENGAN JUGA GUA HAHAHAHA**")
+
+
+@ayiin_cmd(pattern="w(?: |$)(.*)")
+async def _(a):
+    await a.edit("**BABI LU GOBLOK!!CANTIKAN JUGA GUA HAHAHAHA**")
+
+
+@ayiin_cmd(pattern="bct(?: |$)(.*)")
+async def _(a):
+    await a.edit("**BACOTAN LU GAK BIKIN GUA TREMOR GOBLOK HAHAHAHA!!**")
+
+
+@ayiin_cmd(pattern="n(?: |$)(.*)")
+async def _(a):
+    await a.edit("**GIMANA KABAR KAMU SAYANG??APAKAH BAIK?**")
+
+
+@ayiin_cmd(pattern="b(?: |$)(.*)")
+async def _(a):
+    await a.edit("**ELEEEHHHH SOK BANGET KEPINTERAN KAMU!!**")
+
+
+@ayiin_cmd(pattern="m(?: |$)(.*)")
+async def _(y):
+    await y.edit("**INI GRUB APA KUBURAN SEPI BANGET ASTAGFIRULLAH!!**")
+
+
+@ayiin_cmd(pattern="c(?: |$)(.*)")
+async def _(y):
+    await y.edit("**KAN UDAH GUA BILANG??MAKANYA JANGAN NGEYEL!!**")
+
+
+@ayiin_cmd(pattern="x(?: |$)(.*)")
+async def _(a):
+    await a.edit("**GAUSAH SOKAP DEH KAMU!!**")
+
+
+@ayiin_cmd(pattern="v(?: |$)(.*)")
+async def _(y):
+    await y.edit("**MACAM BAGUS AE LU BEGITU HMM!!**")
+
+
+@ayiin_cmd(pattern="j(?: |$)(.*)")
+async def _(i):
+    await i.edit("**MAAF BUKAN JAGOAN HAHAHAHA!!**")
+
+
+@ayiin_cmd(pattern="z(?: |$)(.*)")
+async def _(i):
+    await i.edit("**BISMILLAH SLEEP CALL!!😁**")
+
+
+@ayiin_cmd(pattern="g(?: |$)(.*)")
+async def _(n):
+    await n.edit("**GANTENG LU BEGITU???**")
+
+
+@ayiin_cmd(pattern="yy(?: |$)(.*)")
+async def _(x):
+    await x.edit("**War War Tai anjing, Ketrigger minta sharelok, Udah di sharelok Ga nyamperin,Keras di sosmed Bhakss...**")
+
+@ayiin_cmd(pattern="h(?: |$)(.*)")
+async def _(d):
+    await d.edit("**CANTIK LU BEGITU???**")
+
+
+@ayiin_cmd(pattern="o(?: |$)(.*)")
+async def _(a):
+    await a.edit("**MENTANG MENTANG PUNYA BOT MAINNYA BOT!!PANTES MUKANYA KAYA BOT😁**")
+
+
+@ayiin_cmd(pattern="war(?: |$)(.*)")
+async def _(a):
+    await a.edit("**NORAK LU KONTOL!! MAKE USERBOT CUMAN BUAT WAR MALAH SOK KERAS**")
+
+
+CMD_HELP.update(
+    {
+        "yinstoxic": f"**Plugin : **`yinstoxic`\
+        \n\n  »  **Perintah :** `{cmd}d`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}e`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}f`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}i`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}r`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}t`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}u`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}w`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}z`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}n`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}b`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}m`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}c`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}x`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}v`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}war`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}j`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}g`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}yy`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}h`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}o`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n  »  **Perintah :** `{cmd}bct`\
+        \n  »  **Kegunaan : **Cobain sendiri tod\
+        \n\n**Klo mau Req, kosa kata dari lu Bisa pake Module costum. Ketik** `{cmd}help costum`\
+    "
+    }
+)
